bf911b9c371e5a35b2d4a615ff7265b4598d3def
artvine <74613662+artvinee@users.noreply.github.com>
2024-08-14 18:28:15 +0300

    Update tonsender.py

--- tonsender.py
+++ tonsender.py
@@ -15,31 +15,21 @@ USE_API_KEY = True  # or False, but much more latency and possible overload of t
 API_KEY = ''
 # --------------------------------
 
-async def send_ton_to_address(recipient_address, session):
+
+global seqno
+
+
+async def initialization_wallet():
     mnemonics = SENDER_SEED_PHRASE.split(' ')
 
     version = WalletVersionEnum.v4r2
 
     mnemonics, pub_k, priv_k, wallet = Wallets.from_mnemonics(mnemonics=mnemonics, version=version, workchain=0)
+    return wallet
 
-    if USE_API_KEY == True and API_KEY != '':
-        get_url = f'https://toncenter.com/api/v3/wallet?address={SENDER_ADDRESS}&api_key={API_KEY}'
-        post_url = f'https://toncenter.com/api/v3/message?api_key={API_KEY}'
-    else:
-        get_url = f'https://toncenter.com/api/v3/wallet?address={SENDER_ADDRESS}'
-        post_url = f'https://toncenter.com/api/v3/message'
 
-    async with session.get(get_url) as resp:
-        if resp.status == 200:
-            r = await resp.json()
-            if r['seqno']:
-                seqnoo = r['seqno']
-            else:
-                seqnoo = 0
-        else:
-            print(f'Error for {recipient_address}: {resp.status}')
-
-    amount = round(random.uniform(AMOUNT[0], AMOUNT[1]), 6)
+async def send(session, wallet, recipient_address, seqnoo, post_url):
+    amount = round(random.uniform(AMOUNT[0], AMOUNT[1]), 4)
 
     query = wallet.create_transfer_message(to_addr=recipient_address,
                                            amount=to_nano(float(amount), 'ton'),
@@ -53,22 +43,93 @@ async def send_ton_to_address(recipient_address, session):
     }
     async with session.post(post_url, json=json) as resp:
         if resp.status == 200:
-            print(f'{amount} TON was sent to {recipient_address}.')
+            print(f'Successfully created transaction. Sending {amount} TON to {recipient_address}...')
+            return True, amount
+        else:
+            return False, None
+
+
+async def wait_for_seqno_change(session, get_url, seqno):
+    if USE_API_KEY is True and API_KEY != '':
+        delay = 0.3
+    else:
+        delay = 2
+
+    while True:
+        await asyncio.sleep(delay)
+        if seqno == 0:
+            info = await get_wallet_info(session, get_url)
+            if info['status'] == 'uninit':
+                await asyncio.sleep(delay)
+                continue
+            else:
+                if info['seqno'] > seqno:
+                    return True
+                else:
+                    await asyncio.sleep(delay)
+                    continue
+        else:
+            info = await get_wallet_info(session, get_url)
+            if info['seqno'] > seqno:
+                return True
+            else:
+                await asyncio.sleep(delay)
+                continue
+
+
+async def get_wallet_info(session, get_url):
+    async with session.get(get_url) as response:
+        if response.status == 200:
+            return await response.json()
+        else:
+            print(f'Error getting wallet info: {response.status}, {response.text}, {await response.json()}')
+        return None
 
 
 async def main():
-    with open('wallets.txt', 'r') as f:
+    global seqno
+    if USE_API_KEY is True and API_KEY != '':
+        get_url = f'https://toncenter.com/api/v3/wallet?address={SENDER_ADDRESS}&api_key={API_KEY}'
+        post_url = f'https://toncenter.com/api/v3/message?api_key={API_KEY}'
+    else:
+        get_url = f'https://toncenter.com/api/v3/wallet?address={SENDER_ADDRESS}'
+        post_url = f'https://toncenter.com/api/v3/message'
+
+    with open('wallet.txt', 'r') as f:
         recipient_addresses = [line.strip() for line in f.readlines()]
 
+    wallet = await initialization_wallet()
+
     async with aiohttp.ClientSession() as session:
+        info = await get_wallet_info(session, get_url)
+        await asyncio.sleep(1)
+        if info['status'] == 'uninit':
+            seqno = 0
+        else:
+            seqno = info['seqno']
+
         for recipient_address in recipient_addresses:
             if USE_API_KEY is True and API_KEY != '':
-                await send_ton_to_address(recipient_address, session)
-                await asyncio.sleep(random.uniform(0.5, 0.8))
+                sending, amount = await send(session, wallet, recipient_address, seqno, post_url)
+                if sending is True:
+                    change = await wait_for_seqno_change(session, get_url, seqno)
+                    if change:
+                        seqno += 1
+                    else:
+                        raise Exception(f"Error with {recipient_address}.")
+                    print(f'{amount} TON was sent to {recipient_address}.')
+                await asyncio.sleep(0.1)
             else:
-                await send_ton_to_address(recipient_address, session)
-                await asyncio.sleep(random.uniform(1, 2))
-
+                sending, amount = await send(session, wallet, recipient_address, seqno, post_url)
+                await asyncio.sleep(2)
+                if sending is True:
+                    change = await wait_for_seqno_change(session, get_url, seqno)
+                    if change:
+                        seqno += 1
+                    else:
+                        raise Exception(f"Error with {recipient_address}.")
+                    print(f'{amount} TON was sent to {recipient_address}.')
+                await asyncio.sleep(1)
 
 if __name__ == "__main__":
     asyncio.run(main())
